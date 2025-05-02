3e431b97850855f6186b4ccc0d7d28e93a4e47ae
Tom Winterton <tomwintertoniv@gmail.com>
2025-05-02 13:11:28 -0500

    Added routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { HomepageComponent } from "./Homepage/homepage/homepage.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: HomepageComponent,
+    title: "This is a homepage",
+  },
+];
