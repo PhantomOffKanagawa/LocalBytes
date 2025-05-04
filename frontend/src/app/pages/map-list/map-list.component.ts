f6f6cfef6f9669a6e5b44fcec34803e4caa59430
PhantomOffKanagawa <harry@surmafamily.com>
2025-05-04 18:57:18 -0500

    Set line endings to crlf, lint passing, prettier ran

--- frontend/src/app/pages/map-list/map-list.component.ts
+++ frontend/src/app/pages/map-list/map-list.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { MapViewComponent } from '@components/map/map.component';
 import { ListViewComponent } from '@pages/list-view/list-view.component';
-import { MatCard } from '@angular/material/card';
 
 @Component({
   selector: 'app-map-list',
-  imports: [MapViewComponent, ListViewComponent, MatCard],
+  imports: [MapViewComponent, ListViewComponent],
   templateUrl: './map-list.component.html',
   styleUrl: './map-list.component.css',
 })
