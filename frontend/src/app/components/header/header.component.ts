d53b17061ca2a66d53305e4d83767e3eebad3380
Samantha Whitaker <s.whita6@gmail.com>
2025-05-04 14:24:24 -0500

    minor styling edits to header component

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatToolbar } from '@angular/material/toolbar';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbar],
+  imports: [MatToolbar, MatButton],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
