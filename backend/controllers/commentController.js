ae3e7552d059ae28efe7270b637a68e23f2ef813
PhantomOffKanagawa <harry@surmafamily.com>
2025-05-07 16:53:36 -0500

    Move get uuid to helper util

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -1,25 +1,5 @@
 const Comment = require("../models/Comment");
-const jwt = require("jsonwebtoken");
-const { JWT_SECRET } = require("../utils/config");
-
-// Helper function to get user uuid from token
-const getUserUuidFromToken = (token) => {
-  // Check if token is provided and is a string
-  if (!token || typeof token !== "string") {
-    console.error("Token is missing or invalid");
-    return null;
-  }
-
-  try {
-    // Decode the token using the JWT secret
-    const decoded = jwt.verify(token, JWT_SECRET);
-    // Return the uuid from the decoded token
-    return decoded.uuid;
-  } catch (err) {
-    console.error("Error decoding token:", err.message);
-    return null;
-  }
-};
+const { getUserUuidFromToken } = require("../utils/helpers");
 
 // Create a new comment
 const createComment = async (req, res) => {
