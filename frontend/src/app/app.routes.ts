697d3047270a1e6a8977eb9895c01c620b8aca35
Tom Winterton <tomwintertoniv@gmail.com>
2025-05-07 14:48:04 -0500

    Added basic star component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from '@pages/home/home.component';
 import { ListViewComponent } from '@pages/list-view/list-view.component';
 import { MapListComponent } from '@pages/map-list/map-list.component';
-
+import { StarComponent } from '@components/star/star.component';
 export const routes: Routes = [
   {
     path: '',
